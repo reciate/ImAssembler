ecbb436486ac6b05915abdff7c19ae920064aa3a
reciate <49763556+reciate@users.noreply.github.com>
2021-03-26 23:21:57 +0000

    added directx stuff

--- imassembler/imassembler.cpp
+++ imassembler/imassembler.cpp
@@ -1,5 +1,8 @@
 #include <Windows.h>
 #include <iostream>
+#include <d3d11.h>
+
+#pragma comment (lib, "d3d11.lib")
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch (uMsg) {
@@ -9,6 +12,34 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 	}
 	}
 	return DefWindowProcA(hwnd, uMsg, wParam, lParam);
+}
+
+IDXGISwapChain* swap_chain{};
+ID3D11Device* device{};
+ID3D11DeviceContext* device_context{};
+
+void initialize_dx(HWND hwnd) {
+	DXGI_SWAP_CHAIN_DESC swap_chain_desc { 
+		{
+			0,
+			0,
+			{},
+			DXGI_FORMAT_R8G8B8A8_UNORM
+		},
+		{4},
+		DXGI_USAGE_RENDER_TARGET_OUTPUT,
+		1,
+		hwnd,
+		true
+	};
+
+	D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL, NULL, NULL, D3D11_SDK_VERSION, &swap_chain_desc, &swap_chain, &device, NULL, &device_context);
+}
+
+void cleanup_dx() {
+	device_context->Release();
+	device->Release();
+	swap_chain->Release();
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -22,12 +53,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	ShowWindow(hwnd, nCmdShow);
 
+	initialize_dx(hwnd);
+
 	MSG message{};
 
 	while (GetMessageA(&message, NULL, 0, 0)) {
 		TranslateMessage(&message);
 		DispatchMessageA(&message);
 	}
+
+	cleanup_dx();
 
 	return 0;
 }
